4ab895f4f1425f1de6527aeb41c7f4037b1d9228
ScottGCode <scottrutherfords@gmail.com>
2023-10-20 10:10:27 -0600

    added questions for user input

--- index.js
+++ index.js
@@ -1,13 +1,57 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js'); 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is the name of your project?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Please provide a short description of your project."
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "What are the steps required to install your project?"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide instructions and examples for use."
+    },
+    {
+        type: "input",
+        name: "credits",
+        message: "List your collaborators and their GitHub usernames."
+    },
+    {
+        type: "checkbox",
+        name: "license",
+        message: "Please choose a licesnse",
+        choices: ['MIT', 'GPL', 'WTFPL', 'Apache-2.0']
+    },
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(prcoess.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((inquireAnswers) => {
+        console.log("Success! Generating README!");
+        writeToFile("./utils/generatedREADME.md", generateMarkdown({...inquireAnswers}));
+    })
+}
 
 // Function call to initialize app
 init();
