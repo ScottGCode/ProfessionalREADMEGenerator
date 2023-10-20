6bf179c9150400f99ed2cacb235d734ca2b756d8
ScottGCode <scottrutherfords@gmail.com>
2023-10-20 11:09:57 -0600

    added functions to generate readme

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,62 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license !== "None"){
+    return `![GitHub License](https://img.shields.io/badge/license-${license}-yellowgreen.svg)`
+  }
+  return ""
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license !== "None"){
+    return (`* [License](#license)`)
+  }
+  return ""
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license !== "None") {
+    return (
+      `## License
+      Copyright @ ${license}. All rights reserved.
+      Licensed under the ${license} license.`
+      )
+  }
+  return ""
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+
+  ## Title
+
+  ${data.title}
+  
+  ## Description 
+
+  ${data.description}
+
+  ## Installation
+
+  ${data.intallation}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## Credits
+
+  ${data.credits}
+
+  ## License 
+
+  ${renderLicenseSection(data.license)}
 
 `;
 }
